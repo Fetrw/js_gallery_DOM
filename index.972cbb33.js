b84225d485bf341786cfb3417a1104a942258caa
Fetrw <21vladmel11@gmail.com>
2024-08-14 17:09:45 +0300

    make build

---
+++ index.972cbb33.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").onclick=function(e){e.preventDefault();var t=e.target.closest("a");t&&(document.getElementById("largeImg").src=t.href)};
+//# sourceMappingURL=index.972cbb33.js.map
